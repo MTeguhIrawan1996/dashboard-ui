956501ba6f06b0f6ac0f98f56d78fd254431add8
MTeguhirawan1996 <teguhirawan1996@gmail.com>
2022-10-03 11:16:38 +0700

    Modif

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import "@coreui/coreui/dist/css/coreui.min.css";
 import React, { Suspense } from "react";
 import { HashRouter, Route, Routes } from "react-router-dom";
+// import "@coreui/coreui/dist/css/coreui.min.css";
+// import "./assets/style/style.css";
+import "./assets/style/style.scss";
 
 const loading = (
   <div className="pt-3 text-center">
